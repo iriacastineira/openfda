a271035793dbf5c0443f55e11c2f7d9020257e4d
iriacastineira <37112468+iriacastineira@users.noreply.github.com>
2018-03-09 16:48:17 +0100

    Add files via upload

---
+++ openfda1/show_repos_fda.py
@@ -0,0 +1,17 @@
+import http.client
+import json
+
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json", None, headers)
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+repos_raw = r1.read().decode("utf-8")
+conn.close()
+
+repos = json.loads(repos_raw)
+
+print("The id of the drug is", repos['results'][0]['id'])
+print("The purpose of the drug is", repos['results'][0]['purpose'])
+print("The manufacturer name of the drug is", repos['results'][0]['openfda']['manufacturer_name'])
