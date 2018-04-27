880ff8c1e2b8ba7d7b6566506cd9367b8617ac25
i.castineira.2017 <iriacd27@gmail.com>
2018-04-27 16:05:44 +0200

    upload

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -20,15 +20,14 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         # Send message back to client
 
         # Write content as utf-8 data
-        list = []
-        i = 0
+
 
         path = self.path
         if path == "/":
             with open ("search.html", "r") as f:
                 code = f.read()
                 self.wfile.write(bytes(code, "utf8"))
-        if "search" in path:
+        elif "searchDrug" in path:
             header = {'User-Agent': 'http-client'}
             conn = http.client.HTTPSConnection("api.fda.gov")
             information = path.split("?")[1]
@@ -41,6 +40,19 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             conn.close()
             drug = json.loads(drugs_raw)
             self.wfile.write(bytes(json.dumps(drug),"utf8"))
+        elif "searchCompany" in path:
+            header = {'User-Agent': 'http-client'}
+            conn = http.client.HTTPSConnection("api.fda.gov")
+            information = path.split("?")[1]
+            company = information.split("=")[1].split("&")[0]
+            limit = information.split("&")[1].split("=")[1]
+            url = "/drug/label.json?search=active_ingredient:" + drug + "&" + "limit=" + limit
+            conn.request("GET", url, None, header)
+            r1 = conn.getresponse()
+            drugs_raw = r1.read().decode("utf-8")
+            conn.close()
+            drug = json.loads(drugs_raw)
+            self.wfile.write(bytes(json.dumps(drug), "utf8"))
         return
 
 
